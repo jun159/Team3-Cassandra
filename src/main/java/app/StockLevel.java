d586eade5813e0ec8bec5ee2efd9a8bcea4e2d5c
bobby-lin <bobbylinjh@gmail.com>
2016-09-17 15:24:10 +0800

    Added queries for selecting next available D_NEXT_ID in StockLevel.

--- src/main/java/app/StockLevel.java
+++ src/main/java/app/StockLevel.java
@@ -5,40 +5,64 @@
 
 package app;
 
+import java.util.List;
+
+import com.datastax.driver.core.*;
+
 public class StockLevel {
-	private int w_id;
-	private int d_id;
-	private int stockThreshold;
-	private int numOfLastOrder;
-	private int numOfItemBelowThreshold;
+	private static final String MESSAGE_ITEM_BELOW_THRESHOLD = "Total number of item below threshold (%d) : %d";
+	private static final String DISTRICT_NEXT_AVAILABLE_O_ID = 
+			  "SELECT d_next_o_id"
+			+ "FROM district"
+			+ "WHERE d_w_id = ? and d_id = ?"
+			+ "ORDER BY d_next_o_id DESC"
+			+ "LIMIT 1;";
 	
-	//=============================================================================
-	// Constructor for StockLevel
-	//=============================================================================
-	public StockLevel(int w_id, int d_id, int stockThreshold, int numOfLastOrder) {
-		w_id = this.w_id;
-		d_id = this.d_id;
-		stockThreshold = this.stockThreshold;
-		numOfLastOrder = this.numOfLastOrder;
+	private Session session;
+	private PreparedStatement nextAvailableOrderNum_Select;
+	private Row targetDistrict;
+	
+	public StockLevel(CassandraConnect connect) {
+		this.session = connect.getSession();
+		this.nextAvailableOrderNum_Select = session.prepare(DISTRICT_NEXT_AVAILABLE_O_ID);
 	}
 	
-	public int getNextAvailableOrderNum() {
-		int nextAvailableNum = 0;
+	//====================================================================================
+	// Processing for StockLevel transaction
+	//====================================================================================
+	
+	public void processStockLevel(int w_id, int d_id, int stockThreshold, int numOfLastOrder) {
+		int nextAvailableOrderID = getNextAvailableOrderNum(w_id, d_id);
+		int numOfItemBelowThreshold = countItemBelowThreshold(nextAvailableOrderID);
+		printTotalNumBelowThreshold(stockThreshold, numOfItemBelowThreshold);
+	}
 		
-		// CQL: Retrieve next available number from DB
+	//====================================================================================
+	// CQL: Retrieve next available order number 'D_NEXT_O_ID' for district (W ID,D ID)
+	//====================================================================================
+	public int getNextAvailableOrderNum(int w_id, int d_id) {
+		int nextAvailableNum = 0;
+		ResultSet resultSet = session.execute(nextAvailableOrderNum_Select.bind(w_id, d_id));
+		List<Row> district = resultSet.all();
+		
+		if(!district.isEmpty()) {
+			targetDistrict = district.get(0);
+			nextAvailableNum = targetDistrict.getInt("d_next_o_id");
+		}
 		
 		return nextAvailableNum;
 	}
 	
-	public void countItemBelowThreshold() {
-		numOfItemBelowThreshold = 0;
-		
-		// CQL: Retrieve set of items from last L orders for district (W ID,D ID)
-		
+	//=====================================================================================
+	// CQL: Retrieve set of items from last L orders for district (W ID,D ID)
+	//=====================================================================================
+	public int countItemBelowThreshold(int nextAvailableOrderID) {
+		int countBelowThreshold = 0;
+		return countBelowThreshold;
 	}
 	
-	public void printItemBelowThreshold() {
-		System.out.println(numOfItemBelowThreshold);
+	public void printTotalNumBelowThreshold(int stockThreshold, int numOfItemBelowThreshold) {
+		System.out.println(String.format(MESSAGE_ITEM_BELOW_THRESHOLD, stockThreshold, numOfItemBelowThreshold));
 	}
 	
 }
