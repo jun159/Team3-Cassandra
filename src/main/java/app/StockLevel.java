9c3de4c0bf17238c083cf434c9f912e6f6e0834e
bobby-lin <bobbylinjh@gmail.com>
2016-09-17 13:54:37 +0800

    Added constructor into StockLevel class.

--- src/main/java/app/StockLevel.java
+++ src/main/java/app/StockLevel.java
@@ -6,5 +6,20 @@
 package app;
 
 public class StockLevel {
-
+	private int w_id;
+	private int d_id;
+	private int stockThreshold;
+	private int numOfLastOrder;
+	
+	
+	//=============================================================================
+	// Constructor for StockLevel
+	//=============================================================================
+	public StockLevel(int w_id, int d_id, int stockThreshold, int numOfLastOrder) {
+		w_id = this.w_id;
+		d_id = this.d_id;
+		stockThreshold = this.stockThreshold;
+		numOfLastOrder = this.numOfLastOrder;
+	}
+	
 }
