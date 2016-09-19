f7a301a2e0d5bf2c0a385c74558b48a715342002
bobby-lin <bobby.linjh@gmail.com>
2016-09-19 20:31:37 +0800

    Added query to TopBalance

--- src/main/java/app/TopBalance.java
+++ src/main/java/app/TopBalance.java
@@ -6,28 +6,53 @@
 
 package app;
 
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.Session;
+
 public class TopBalance {
-	private String MESSAGE_CUSTOMER_NAME = "Name of Customer: %s";
-	private String MESSAGE_CUSTOMER_BALANCE = "Customer Balance: %s";
-	private String MESSAGE_WAREHOUSE_NAME_OF_CUSTOMER = "Warehouse Name of Customer: %s";
-	private String MESSAGE_DISTRICT_NAME_OF_CUSTOMER = "District Name of Customer: %s";
+	private static final String MESSAGE_CUSTOMER_NAME = "Name of Customer: %s";
+	private static final String MESSAGE_CUSTOMER_BALANCE = "Customer Balance: %2f";
+	private static final String MESSAGE_WAREHOUSE_NAME_OF_CUSTOMER = "Warehouse Name of Customer: %s";
+	private static final String MESSAGE_DISTRICT_NAME_OF_CUSTOMER = "District Name of Customer: %s";
+
+	//====================================================================================
+	// CQL Queries for TopBalance transaction
+	//====================================================================================
+	private static final String CUSTOMER_SELECT = 
+	          "SELECT c_balance, c_w_id, c_d_id, c_first, c_middle, c_last"
+			+ "FROM customer;";
 	
 	
-	
-	
-	
+	//====================================================================================
+	// Preparing for session
+	//====================================================================================
+
+	private Session session;
+
+	public TopBalance(CassandraConnect connect) {
+		this.session = connect.getSession();
+	}
+
+	//====================================================================================
+	// Processing Top Balance transaction
+	//====================================================================================
+
+	public void processTopBalance() {
+		
+	}
+
 	private void printCustomerName(String customerName) {
 		System.out.printf(MESSAGE_CUSTOMER_NAME, customerName);
 	}
-	
+
 	private void printCustomerBalance(float balance) {
 		System.out.printf(MESSAGE_CUSTOMER_BALANCE, balance);
 	}
-	
+
 	private void printWarehouseNameOfCustomer(String warehouseName) {
 		System.out.printf(MESSAGE_WAREHOUSE_NAME_OF_CUSTOMER, warehouseName);
 	}
-	
+
 	private void printDistrictNameOfCustomer(String districtName) {
 		System.out.printf(MESSAGE_DISTRICT_NAME_OF_CUSTOMER, districtName);
 	}
